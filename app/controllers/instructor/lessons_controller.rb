03e391e932be54a7585dbfb2535ed0847c226559
Alex Adamo <illuzzi0nz10@gmail.com>
2016-02-19 07:16:22 +0000

    locked down the instructors/sections pages

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -11,10 +11,8 @@ class Instructor::LessonsController < ApplicationController
     redirect_to instructor_course_path(current_section.course)
   end
 
-  helper_method :current_section
-  def current_section
-    @current_section ||= Section.find(params[:section_id])
-  end
+
+  private
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
@@ -22,7 +20,10 @@ class Instructor::LessonsController < ApplicationController
     end
   end
 
-  private
+  helper_method :current_section
+  def current_section
+    @current_section ||= Section.find(params[:section_id])
+  end
 
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle)
